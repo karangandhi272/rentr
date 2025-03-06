4794b0dcfffb48766dfc25121a5cb6bd6d92408a
Mika Vohl <mikavohl@gmail.com>
2025-03-06 14:53:22 -0500

    Fixed auth styling

--- rentr/src/features/auth/AuthenticationPage.tsx
+++ rentr/src/features/auth/AuthenticationPage.tsx
@@ -270,15 +270,16 @@ const AuthenticationPage = () => {
                 </div>
               </form>
               <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <span className="w-full border-t" />
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span className="bg-background px-2 text-muted-foreground">
-                    Or continue with
-                  </span>
-                </div>
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
               </div>
+
+              <div className="relative flex justify-center text-xs uppercase z-10">
+                <span className="bg-white px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
               <Button
                 type="button"
                 variant="outline"
